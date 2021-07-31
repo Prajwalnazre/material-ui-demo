7f9b33f24f0b541fa34053f010da84cb176a83a8
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-31 20:20:44 +0530

    feature: New buttons explored

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import  React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, Cssbaseline, Grid, Container, Toolbar, CssBaseline } from '@material-ui/core'
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, Cssbaseline, Grid, Container, Toolbar, CssBaseline } from '@material-ui/core'
 import { PhotoCamera } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+
+}));
 
 const App = () => {
     return (
@@ -20,6 +25,21 @@ const App = () => {
                         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Photo Album
                         </Typography>
+                        <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis vitae voluptatum optio expedita laborum voluptate commodi quisquam libero repellat nemo cum, repellendus atque maiores corrupti harum eveniet, omnis, facere ratione.
+                        </Typography>
+                        <Grid container spacing={2} justify="center"> 
+                            <Grid item>
+                                <Button variant="contained" color="primary">
+                                    See my photos
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="outlined" color="primary">
+                                    Secondary Action
+                                </Button>
+                            </Grid>
+                        </Grid>
                     </Container>
                 </div>
             </main>
