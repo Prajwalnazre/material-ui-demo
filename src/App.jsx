bf087d583f74b216a1297120acc59918c837572e
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-08-01 11:33:17 +0530

    feature: Image card created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import  React from 'react';
+import  React, { useState } from 'react';
 import clsx from 'clsx';
 
 import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, Cssbaseline, Grid, Container, Toolbar, CssBaseline } from '@material-ui/core';
 import { CardHeader, Collapse, Avatar, IconButton } from '@material-ui/core';
-import { PhotoCamera, FavoriteIcon, ShareIcon, ExpandMoreIcon, MoreVertIcon } from '@material-ui/icons';
+import { PhotoCamera } from '@material-ui/icons';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleClick = () => {
+        setExpanded(!expanded);
+    }
+
     return (
         <>
             <CssBaseline />
@@ -48,11 +58,66 @@ const App = () => {
                         <Grid Container spacing={2} justify="center" sm={12} md={6} lg={4}>
                             <Grid item>
                                 <Card>
-                                    <CardHeader />
+                                    <CardHeader 
+                                        avatar = {
+                                            <Avatar aria-label="random_person" className={classes.avatar}>
+                                                P
+                                            </Avatar>
+                                        }
+                                        action={
+                                            <IconButton aria-label="settings">
+                                                <MoreVertIcon />
+                                            </IconButton>
+                                        }
+                                        title="Random Image Below"
+                                        subheader="Random subheader here"
+                                    />
                                     <CardMedia className={classes.image} 
                                         image="https://source.unsplash.com/random"
                                         title="random_image"
                                     />
+                                    <CardContent>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            Random Text again! Lorem ipsum dolor sit, amet consectetur 
+                                            adipisicing elit. Sapiente, laborum ad distinctio officiis 
+                                            tenetur cum sunt accusamus saepe odio recusandae.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions disableSpacing>
+                                        <IconButton aria-label="Add to Favourites">
+                                            <FavoriteIcon />
+                                        </IconButton>
+                                        <IconButton aria-label="Share">
+                                            <ShareIcon />
+                                        </IconButton>
+                                        <IconButton
+                                            className={clsx(classes.expand, {
+                                                [classes.expandOpen]:expanded,
+                                            })}
+                                            onClick={handleClick}
+                                            aria-expanded={expanded}
+                                            aria-label="Show More"
+                                        >
+                                            <ExpandMoreIcon />
+                                        </IconButton>
+                                    </CardActions>
+                                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                        <CardContent>
+                                            <Typography paragraph>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                                Ipsa veritatis minima ipsam, autem reiciendis qui 
+                                                repellendus est facilis, aspernatur a natus voluptatem 
+                                                nulla, voluptates repudiandae.
+                                            </Typography>
+                                            <Typography paragraph>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                Provident nam est facilis culpa eligendi explicabo 
+                                                numquam ipsam quaerat temporibus quas, animi doloremque 
+                                                alias officiis debitis quod ex, autem doloribus 
+                                                molestias!
+                                            </Typography>
+                                        </CardContent>
+                                    </Collapse>
                                 </Card>
                             </Grid>
                         </Grid>
--- src/styles.js
+++ src/styles.js
@@ -15,7 +15,21 @@ const useStyles = makeStyles((theme) => ({
     image : {
         height: 0,
         paddingTop: "56.25%",
+    },
+    expand : {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+    })
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)'
+    },
+    avatar : {
+        backgroundColor: 'rgba(0, 100, 200)',
     }
+
 }));
 
 export default useStyles;
